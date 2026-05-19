8018dc5ed7b467509882aaa33892ca1e886ebc4c
rosetwala45-oss <rosetwala45@gmail.com>
2026-05-19 22:39:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let homeScore = 0;
+let guestScore = 0;
+// let score = 0;
+
+function addOne() {}
+
+function addTwo() {}
+
+function addThree() {}
+
+let homeScoreEl = document.getElementById("home-score");
+homeScoreEl.textContent = " ";
+
+let guestScoreEl = document.getElementById("guest-score");
+guestScoreEl.textContent = " ";
